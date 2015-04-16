362637fe2d2cc01cfee8d2c2110adb990d357188
MinasMazar <minasmazar@gmail.com>
2015-04-16 21:27:39 +0200

    Version 1.0 - it sounds bells for current time.

--- lib/pescobells.rb
+++ lib/pescobells.rb
@@ -1,5 +1,34 @@
 require "pescobells/version"
 
 module Pescobells
-  # Your code goes here...
+
+  SOUND_COMMAND = ' {player} {pathname} >/dev/null 2>/dev/null '
+  BELLS_SOUNDS = {
+    :hour => File.expand_path('../../assets/hour_bell.wav', __FILE__),
+    :quarter => File.expand_path('../../assets/quarter_bell.wav', __FILE__),
+  }
+
+  attr_accessor :player_executable
+
+  def time_to_bells(time = Time.now)
+    bells = [ [], [] ]
+    ( time.hour % 12 ).times do
+      bells[0] << BELLS_SOUNDS[:hour]
+    end
+    ( time.min / 15 ).times do
+      bells[1] << BELLS_SOUNDS[:quarter]
+    end
+    bells
+  end
+
+  def play_bells(bells)
+    bells.flatten.map do |bell|
+      play_sound self.player_executable, bell
+    end
+  end
+
+  def play_sound(player, audio_file)
+    system SOUND_COMMAND.sub('{player}', player).sub('{pathname}', audio_file)
+  end
+    
 end
--- test/test_pescobells.rb
+++ test/test_pescobells.rb
@@ -1,11 +1,25 @@
-require 'minitest_helper'
+require 'minitest/autorun'
+require 'pescobells'
 
 class TestPescobells < Minitest::Test
+
+  include Pescobells
+
   def test_that_it_has_a_version_number
     refute_nil ::Pescobells::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_calculate_bells
+    time = Time.now
+    bells = time_to_bells time 
+    assert_equal bells[0].size, time.hour % 12
+    assert_equal bells[1].size, time.min / 15  
   end
+
+  def test_playing_bells
+    time = Time.now
+    bells = time_to_bells time
+    assert_equal [ true ],  play_bells(bells).uniq
+  end
+
 end
